5734a14b23986aef008436fad639e61f634d89d9
vintinls <victorlsboa@gmail.com>
2024-06-09 18:33:45 -0300

    commit: header pronto

--- Trab-Final-Grupo3/src/App.jsx
+++ Trab-Final-Grupo3/src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Header from './components/Header'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <Header/>
+      <p>teste</p>
+    </div>
   )
 }
 
--- Trab-Final-Grupo3/src/components/Header/index.jsx
+++ Trab-Final-Grupo3/src/components/Header/index.jsx
@@ -1,11 +1,21 @@
-import './style.css'
+import React from 'react';
+import './style.css'; // Arquivo CSS separado para estilos
+import logo from '../../assets/logoSite.png';
 
-import React from 'react'
+const Header = () => {
+    const createAd = () => {
+        window.location.href = '/pagina-anuncio'; // Mude para a rota desejada
+    };
 
-function Header() {
-  return (
-    <div>Header</div>
-  )
-}
+    return (
+        <header className="header">
+            <div className="logo">
+                <img src={logo} alt="Logo" />
+                <div className="text">Serra Market</div>
+            </div>
+            <button className="button" onClick={createAd}>Criar um anúncio</button>
+        </header>
+    );
+};
 
-export default Header
+export default Header;

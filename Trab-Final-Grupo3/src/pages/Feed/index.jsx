a4567e5f300353fdbb03e0641ada0ce8383d140f
vintinls <victorlsboa@gmail.com>
2024-06-11 16:00:25 -0300

    commit: estilizaçao

--- Trab-Final-Grupo3/src/pages/Feed/index.jsx
+++ Trab-Final-Grupo3/src/pages/Feed/index.jsx
@@ -1,8 +1,8 @@
-import Header from "../../components/Header";
-import "./style.css";
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import Header from '../../components/Header';
+import './style.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Feed() {
   const [produtos, setProdutos] = useState([]);
@@ -13,53 +13,51 @@ function Feed() {
 
   const fetchProdutos = async () => {
     try {
-      const response = await axios.get("http://localhost:8080/api/produtos");
+      const response = await axios.get('http://localhost:8080/api/produtos');
       setProdutos(response.data);
     } catch (error) {
-      console.error("Erro ao buscar produtos:", error);
+      console.error('Erro ao buscar produtos:', error);
     }
   };
 
-  const deletarProduto = async (id) => {
+  const deleteProduto = async (id) => {
     try {
       await axios.delete(`http://localhost:8080/api/produtos/${id}`);
-      setProdutos(produtos.filter((produto) => produto.id != id));
+      setProdutos(produtos.filter((produto) => produto.id !== id));
     } catch (error) {
-      console.error("Erro ao deletar produto:", error);
+      console.error('Erro ao deletar produto:', error);
     }
-    // function deletePost(id) {
-    //   axios.delete(`https://665fa6d55425580055b0594f.mockapi.io/posts/${id}`);
-    //   setPosts(posts.filter((post) => post.id != id));
-    // }
   };
 
   return (
     <div>
       <Header />
       <main>
-        <div className="cards">
+        <div className='cards'>
           {produtos.map((produto) => (
-            <div key={produto.id} className="card">
+            <div key={produto.id} className='card'>
               <header>
                 <h2>{produto.titulo}</h2>
               </header>
-              <div className="line" />
+              <div className='line' />
               <p>{produto.descricao}</p>
-              <div className="btns">
-                <div className="btn-edit">
+              <div className='btns'>
+                <div className='btn-edit'>
                   <Link to={`/update/${produto.id}`}>
                     <button>Editar</button>
                   </Link>
                 </div>
 
-                <div className="btn-more">
+                <div className='btn-more'>
                   <Link to={`/more/${produto.id}`}>
                     <button>More</button>
                   </Link>
                 </div>
 
-                <div className="btn-delete">
-                <button onClick={() => deleteProduto(produto.id)}>Apagar</button>
+                <div className='btn-delete'>
+                  <button onClick={() => deleteProduto(produto.id)}>
+                    Apagar
+                  </button>
                 </div>
               </div>
             </div>
